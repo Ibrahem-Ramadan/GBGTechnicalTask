574f4ce927092972fbcb4fc1534c461fa0bd5aaf
Ibrahem-Ramadan <ibrahem.ramadan@cis.asu.edu.eg>
2023-07-05 02:35:26 +0300

    Adding Course Api with single endpoint GetCourseById

---
+++ GBGTechnicalTask.Api/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using GBGTechnicalTask.Core.Features.Courses.Queries.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GBGTechnicalTask.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CourseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCourseById(int id)
+        {
+            var response= await _mediator.Send(new GetCourseByIdQuery{ id=id });
+            return Ok(response);
+        }
+    }
+}
