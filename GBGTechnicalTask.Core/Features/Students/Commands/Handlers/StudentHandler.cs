71a8d6c9d055f8fc3867a77b81aa3b48e2782f22
Ibrahem-Ramadan <ibrahem.ramadan@cis.asu.edu.eg>
2023-07-05 05:17:32 +0300

    Adding student handler and controller

---
+++ GBGTechnicalTask.Core/Features/Students/Commands/Handlers/StudentHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using GBGTechnicalTask.Core.Features.Students.Commands.Models;
+using GBGTechnicalTask.Data.Entities;
+using GBGTechnicalTask.Service.IServices;
+using MediatR;
+
+namespace GBGTechnicalTask.Core.Features.Students.Commands.Handlers
+{
+    public class StudentHandler : IRequestHandler<AddStudentCommand, Student>
+    {
+        private readonly IStudentService _studentService;
+        private readonly IMapper _mapper;
+
+        public StudentHandler(
+              IStudentService studentService
+            , IMapper mapper)
+        {
+            _studentService = studentService;
+            _mapper = mapper;
+        }
+        public Task<Student> Handle(AddStudentCommand request, CancellationToken cancellationToken)
+        {
+            var studentMapped= _mapper.Map<Student>(request);
+            return _studentService.AddStudentAsync(studentMapped);
+        }
+    }
+}
