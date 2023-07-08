714026285ea47d6848639dd443e71500b1afe4f5
Ibrahem-Ramadan <ibrahem.ramadan@cis.asu.edu.eg>
2023-07-08 07:40:55 +0300

    add validator based on fluent validator

---
+++ GBGTechnicalTask.Core/Features/Enrollments/Commands/Validators/AddEnrollmentValidator.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using GBGTechnicalTask.Core.Features.Enrollments.Commands.Models;
+using GBGTechnicalTask.Data.Entities;
+using GBGTechnicalTask.Service.IServices;
+using Microsoft.AspNetCore.Connections.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBGTechnicalTask.Core.Features.Enrollments.Commands.Validators
+{
+    public class AddEnrollmentValidator:AbstractValidator<AddEnrollmentCommand>
+    {
+        private readonly IEnrollmentService _enrollmentService;
+
+        public AddEnrollmentValidator(IEnrollmentService enrollmentService)
+        {
+            ApplyValidationsRules();
+            ApplyCustomValidationsRules();
+            _enrollmentService = enrollmentService;
+        }
+
+        public void ApplyValidationsRules()
+        {
+            RuleFor(e => e.CourseId)
+                .NotEmpty().WithMessage("{PropertyName} is Required")
+                .NotNull().WithMessage("{PropertyName} is Required");
+
+            RuleFor(e => e.StudentId)
+                .NotEmpty().WithMessage("{PropertyName} is Required")
+                .NotNull().WithMessage("{PropertyName} is Required");
+        }
+        public void ApplyCustomValidationsRules()
+        {
+            RuleFor(e =>
+                new Enrollment
+                {
+                    CourseId = e.CourseId,
+                    StudentId = e.StudentId
+                }).MustAsync(async (Enrollment, CancellationToken) => !await _enrollmentService.IsStudentAlreadyRegisteredToTheCourse(Enrollment))
+                .WithMessage("Student Already Registered to this Course");
+        }
+    }
+}
