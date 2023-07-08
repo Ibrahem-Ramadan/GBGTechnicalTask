4913d4b4f737236bdaf577b7bf7f9de173950d03
Ibrahem-Ramadan <ibrahem.ramadan@cis.asu.edu.eg>
2023-07-08 07:34:24 +0300

    override controller base response

---
+++ GBGTechnicalTask.Api/Controllers/AppControllerBase.cs
@@ -0,0 +1,37 @@
+﻿using GBGTechnicalTask.Core.Bases.Response;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace GBGTechnicalTask.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppControllerBase : ControllerBase
+    {
+        public new ObjectResult Response<T>(Response<T> response)
+        {
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                    return new OkObjectResult(response);
+                case HttpStatusCode.Created:
+                    return new CreatedResult(string.Empty, response);
+                case HttpStatusCode.Unauthorized:
+                    return new UnauthorizedObjectResult(response);
+                case HttpStatusCode.BadRequest:
+                    return new BadRequestObjectResult(response);
+                case HttpStatusCode.NotFound:
+                    return new NotFoundObjectResult(response);
+                case HttpStatusCode.Accepted:
+                    return new AcceptedResult(string.Empty, response);
+                case HttpStatusCode.UnprocessableEntity:
+                    return new UnprocessableEntityObjectResult(response);
+                default:
+                    return new BadRequestObjectResult(response);
+            }
+        }
+
+    }
+}
