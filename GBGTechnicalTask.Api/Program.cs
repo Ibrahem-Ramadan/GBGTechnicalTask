ea2f72423d4dd337b05550de1704074b97ac534c
Ibrahem-Ramadan <ibrahem.ramadan@cis.asu.edu.eg>
2023-07-08 07:50:09 +0300

    configure serilog/exceptions  middleware/data seeding

--- GBGTechnicalTask.Api/Program.cs
+++ GBGTechnicalTask.Api/Program.cs
@@ -1,17 +1,21 @@
 using GBGTechnicalTask.Core;
+using GBGTechnicalTask.Core.Middleware;
 using GBGTechnicalTask.Infrastructure;
 using GBGTechnicalTask.Infrastructure.Data;
 using GBGTechnicalTask.Service;
+using GBGTechnicalTask.Service.IServices;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Exceptions;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSerilog();
+builder.Services.AddControllers().AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(optionBuilder =>
 {
     optionBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DBConn"));
@@ -22,6 +26,13 @@ builder.Services
     .AddServiceDependencies()
     .AddCoreDependencies();
 
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .Enrich.WithExceptionDetails()
+    .Enrich.WithMachineName()
+    .CreateLogger();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,11 +41,32 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler();
+}
 
+app.UseSerilogRequestLogging();
+app.UseMiddleware<ExceptionHandlerMiddleware>();
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
-app.Run();
+try
+{
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.EnsureCreatedAsync();
+
+    var seeder = scope.ServiceProvider.GetRequiredService<IDataSeederService>();
+    await seeder.SeedData();
+
+    Log.Information("Application starting up...");
+    app.Run();
+    Log.Information("Application was shutdown");
+}catch (Exception ex){
+    Log.Fatal(ex, "Application host terminated unexpectedly");
+}finally{
+    Log.CloseAndFlush();
+}
